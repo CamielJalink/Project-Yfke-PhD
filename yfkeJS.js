98dfae7aba0c81f54805b2e9bd31dd4f30e6b76f
CamielJalink <camiel.jalink@gmail.com>
2018-08-14 17:45:52 +0200

    Indonesia vacation edits: first expand/collapse logic

---
+++ yfkeJS.js
@@ -0,0 +1,23 @@
+// All document objects needed for the site's behavior.
+var allAbstracts = document.querySelectorAll(".abstract");
+var allAbstractIcons = document.querySelectorAll(".abstractIcon");
+var allAbstractTexts = document.querySelectorAll(".abstractTextHidden");
+
+
+// Code linking abstract icon and abstract test-button together. 
+for(var i = 0; i < allAbstracts.length; i++){
+	allAbstracts[i].abstractNumber = i;
+	allAbstracts[i].addEventListener("click", abstractClicked);
+}
+
+
+// Abstract eventhandler for showing and hiding the abstracts.
+function abstractClicked(){
+	var icon = allAbstractIcons[this.abstractNumber];
+	var text = allAbstractTexts[this.abstractNumber];
+
+  icon.classList.toggle("fa-angle-right");
+	icon.classList.toggle("fa-angle-down");
+	text.classList.toggle("abstractTextHidden");
+	text.classList.toggle("abstractTextVisible");
+}
